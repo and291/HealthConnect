081311a78d6f189123b3fbbfd6fddc37ff4ceec3
Andrey Busik <and2919@gmail.com>
2025-08-22 16:07:11 +0300

    Check if records are unique

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.material3.adaptive.navigation3)
     implementation(libs.kotlinx.serialization.core)
+
+    testImplementation(libs.junit)
 }
