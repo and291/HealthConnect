fa0642f77a20823d48e50140727fef04160e47c4
Andrey Busik <and2919@gmail.com>
2025-11-26 16:13:13 +0300

    Add ActiveCaloriesBurnedEditor

---
+++ app/editor/impl/src/main/java/com/example/healthconnect/editor/impl/ui/editor/ActiveCaloriesBurnedEditor.kt
@@ -0,0 +1,80 @@
+package com.example.healthconnect.editor.impl.ui.editor
+
+import androidx.health.connect.client.records.ActiveCaloriesBurnedRecord
+import androidx.health.connect.client.records.metadata.Metadata
+import androidx.health.connect.client.units.calories
+import com.example.healthconnect.components.api.ui.model.TimeComponentModel
+import com.example.healthconnect.components.api.ui.model.ValueComponentModel
+import com.example.healthconnect.editor.api.ui.mapper.MetadataMapper
+import com.example.healthconnect.editor.api.ui.model.ActiveCaloriesBurnedModel
+import com.example.healthconnect.editor.api.ui.model.ModelModificationEvent
+import java.time.Instant
+import java.time.ZoneOffset
+
+class ActiveCaloriesBurnedEditor() : Editor<ActiveCaloriesBurnedRecord, ActiveCaloriesBurnedModel>() {
+
+    @Suppress("REDUNDANT_ELSE_IN_WHEN")
+    override fun update(
+        model: ActiveCaloriesBurnedModel,
+        event: ModelModificationEvent,
+    ): ActiveCaloriesBurnedModel = when (event) {
+        is ModelModificationEvent.OnMetadataChanged -> model.copy(
+            metadata = event.metadata
+        )
+
+        is ModelModificationEvent.OnTimeChanged -> model.copy(
+            time = event.time
+        )
+
+        is ModelModificationEvent.OnValueChanged -> when (event.value.type) {
+            is ValueComponentModel.Type.Energy -> model.copy(
+                energy = event.value
+            )
+
+            else -> throw NotImplementedError()
+        }
+
+        else -> throw NotImplementedError()
+    }
+
+    override fun toModel(
+        record: ActiveCaloriesBurnedRecord,
+        mapper: MetadataMapper,
+    ): ActiveCaloriesBurnedModel = ActiveCaloriesBurnedModel(
+        time = TimeComponentModel.Interval(
+            startTime = record.startTime,
+            startZoneOffset = record.startZoneOffset,
+            endTime = record.endTime,
+            endZoneOffset = record.endZoneOffset
+        ),
+        metadata = mapper.toEntity(record.metadata),
+        energy = ValueComponentModel.Dbl(
+            parsedValue = record.energy.inCalories,
+            type = ValueComponentModel.Type.Energy(),
+        ),
+    )
+
+    override fun toRecord(
+        validModel: ActiveCaloriesBurnedModel,
+        mapper: MetadataMapper,
+    ): ActiveCaloriesBurnedRecord = ActiveCaloriesBurnedRecord(
+        startTime = validModel.getStartValidTime().instant,
+        startZoneOffset = validModel.getStartValidTime().zoneOffset,
+        endTime = validModel.getEndValidTime().instant,
+        endZoneOffset = validModel.getEndValidTime().zoneOffset,
+        metadata = mapper.toLibMetadata(validModel.metadata),
+        energy = (validModel.energy as ValueComponentModel.Dbl).parsedValue!!.calories,
+    )
+
+    override fun createDefault(): ActiveCaloriesBurnedRecord {
+        val instant = Instant.now()
+        return ActiveCaloriesBurnedRecord(
+            startTime = instant.minusSeconds(60),
+            startZoneOffset = ZoneOffset.UTC,
+            endTime = instant,
+            endZoneOffset = ZoneOffset.UTC,
+            metadata = Metadata.Companion.unknownRecordingMethod(),
+            energy = 60.calories,
+        )
+    }
+}
