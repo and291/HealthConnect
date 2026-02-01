bff097c0a6db278473f80f4b08c7c6e19c252600
Andrey Busik <and2919@gmail.com>
2026-02-01 12:59:51 +0300

    existing sub components

--- app/components/api/src/main/java/com/example/healthconnect/components/api/ui/model/ComponentModel.kt
+++ app/components/api/src/main/java/com/example/healthconnect/components/api/ui/model/ComponentModel.kt
@@ -1,10 +1,6 @@
 package com.example.healthconnect.components.api.ui.model
 
-import java.util.UUID
+interface ComponentModel {
 
-sealed class ComponentModel(
-    open val presentationId: UUID
-) {
-
-    abstract fun isValid(): Boolean
+    fun isValid(): Boolean
 }
