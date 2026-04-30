66bac36e82b7cde0c68cf28836a659043266075a
Andrey Busik <and2919@gmail.com>
2026-04-30 17:48:34 +0700

    add permission modules

---
+++ app/permissions/api/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.kotlin.dsl.configure
+
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+extensions.configure<LibraryExtension> {
+    namespace = "com.example.healthconnect.permissions.api"
+    compileSdk = 36
+    compileSdkExtension = 19
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
+}
+
+dependencies {
+    implementation(project(":app:navigation:api"))
+
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.navigation3.runtime)
+
+    testImplementation(libs.junit)
+}
