99c1c224f3449814ad0e2e93d81e80ea914bf969
Andrey Busik <and2919@gmail.com>
2026-05-02 20:41:23 +0700

    remove old permissions indicator

--- app/src/main/java/com/example/healthconnect/ui/ParameterlessViewModelFactory.kt
+++ app/src/main/java/com/example/healthconnect/ui/ParameterlessViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.example.healthconnect.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.healthconnect.domain.LibraryRepository
-import com.example.healthconnect.ui.screen.SdkPermissionsViewModel
 
 //TODO change naming as current is not actually correct: this single VM requires constructor parameter
 class ParameterlessViewModelFactory(
@@ -14,7 +13,6 @@ class ParameterlessViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when (modelClass) {
             ActivityViewModel::class.java -> ActivityViewModel(libraryRepository)
-            SdkPermissionsViewModel::class.java -> SdkPermissionsViewModel(libraryRepository)
             else -> throw IllegalStateException("Unknown ViewModel class:" + modelClass.canonicalName)
         } as T
     }
